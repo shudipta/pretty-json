c548da07c8cbc27584abf018c7f2ca0df3e17896
shudipta <alittleprogramming@gmail.com>
2020-04-25 15:46:59 +0600

    fix bugs
    - fix bugs
    - update formatting logic

--- pretty_json.go
+++ pretty_json.go
@@ -7,11 +7,13 @@ import (
 	"strings"
 )
 
-var indentationChar string
+var indentationStr string
 
 func init() {
-	indentationChar = "  "
+	indentationStr = "  "
 }
+
+type Interface interface {}
 
 func mustBe(v interface{}, err error) interface{} {
 	if err != nil {
@@ -32,13 +34,14 @@ func stringify(s string) string {
 func indent(iskey bool, ind int, v reflect.Value) {
 	if iskey {
 		for i := 0; i < ind; i++ {
-			fmt.Print(indentationChar)
+			fmt.Print(indentationStr)
 		}
 	}
 	fmt.Print(v)
 }
 
 func rec(v reflect.Value, iskey bool, ind int) {
+	//fmt.Println("++++++++++", v.Kind())
 	if v.IsZero() {
 		switch v.Kind() {
 		case reflect.Interface, reflect.Ptr, reflect.Map,
@@ -49,14 +52,11 @@ func rec(v reflect.Value, iskey bool, ind int) {
 	}
 
 	switch v.Kind() {
-	case reflect.Bool,
-		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
-		reflect.Float32, reflect.Float64:
-		indent(iskey, ind, v)
-
-	case reflect.String:
-		indent(iskey, ind, reflect.ValueOf(stringify(v.String())))
+	//case reflect.Bool,
+	//	reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+	//	reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+	//	reflect.Float32, reflect.Float64:
+	//	indent(iskey, ind, v)
 
 	case reflect.Interface:
 		rec(v.Elem(), iskey, ind)
@@ -69,10 +69,12 @@ func rec(v reflect.Value, iskey bool, ind int) {
 		rec(vv, iskey, ind)
 
 	case reflect.Map:
-		indent(false, ind, reflect.ValueOf("{\n"))
+		indent(false, ind, reflect.ValueOf("{"))
+		hasElem := len(v.MapKeys()) > 0
 		iter := v.MapRange()
 		cond := iter.Next()
 		for cond {
+			fmt.Println()
 			key := iter.Key()
 			rec(key, true, ind+1)
 			fmt.Print(": ")
@@ -80,63 +82,73 @@ func rec(v reflect.Value, iskey bool, ind int) {
 			rec(val, false, ind+1)
 			cond = iter.Next()
 			if cond {
-				fmt.Println(",")
+				fmt.Print(",")
 			}
 		}
-		fmt.Println()
-		indent(true, ind, reflect.ValueOf("}"))
-
-	case reflect.Array, reflect.Slice:
-		indent(false, ind, reflect.ValueOf("[\n"))
-		for i := 0; i < v.Len(); i++ {
-			rec(v.Index(i), true, ind+1)
-			if i < v.Len()-1 {
-				fmt.Println(",")
-			}
-		}
-		if v.Len() > 0 {
+		if hasElem {
 			fmt.Println()
 		}
-		indent(true, ind, reflect.ValueOf("]"))
+		indent(hasElem, ind, reflect.ValueOf("}"))
+
+	case reflect.Array, reflect.Slice:
+		indent(false, ind, reflect.ValueOf("["))
+		hasElem := v.Len() > 0
+		for i := 0; i < v.Len(); i++ {
+			fmt.Println()
+			rec(v.Index(i), true, ind+1)
+			if i < v.Len()-1 {
+				fmt.Print(",")
+			}
+		}
+		if hasElem {
+			fmt.Println()
+		}
+		indent(hasElem, ind, reflect.ValueOf("]"))
 
 	case reflect.Struct:
-		indent(false, ind, reflect.ValueOf("{\n"))
+		indent(false, ind, reflect.ValueOf("{"))
 		t := v.Type()
+		hasElem := false
 		for i := 0; i < v.NumField(); i++ {
 			ft := t.Field(i)
 			fv := v.Field(i)
 
-			tagName := strings.TrimSpace(ft.Tag.Get("json"))
-			if tagName == "" || tagName == "-" {
-				continue
+			tagName, hasTag := ft.Tag.Lookup("json")
+			if hasTag {
+				tagName = strings.TrimSpace(tagName)
+				if tagName == "" || tagName == "-" {
+					continue
+				}
 			}
 
-			if fv.IsZero() && fv.IsValid() && fv.CanSet() {
-				//if fv.IsValid() && fv.CanSet() {
-				if d, ok := ft.Tag.Lookup("default"); ok && d != "" {
-					//fmt.Print(d)
-					if fv.Kind() == reflect.String {
-						fv.SetString(d)
-					} else if fv.Kind() == reflect.Bool {
-						fv.SetBool(mustBe(strconv.ParseBool(d)).(bool))
-					} else if fv.Kind() == reflect.Int ||
-						fv.Kind() == reflect.Int8 ||
-						fv.Kind() == reflect.Int16 ||
-						fv.Kind() == reflect.Int32 ||
-						fv.Kind() == reflect.Int64 {
-						fv.SetInt(mustBe(strconv.ParseInt(d, 0, 64)).(int64))
-					} else if fv.Kind() == reflect.Uint ||
-						fv.Kind() == reflect.Uint8 ||
-						fv.Kind() == reflect.Uint16 ||
-						fv.Kind() == reflect.Uint32 ||
-						fv.Kind() == reflect.Uint64 {
-						fv.SetUint(mustBe(strconv.ParseUint(d, 0, 64)).(uint64))
-					} else if fv.Kind() == reflect.Float32 ||
-						fv.Kind() == reflect.Float64 {
-						fv.SetFloat(mustBe(strconv.ParseFloat(d, 64)).(float64))
-					} else if d == "null" {
-						//fv.Set(reflect.New(fv.Type()).Elem())
-						fv.Set(reflect.New(ft.Type).Elem())
+			if fv.IsZero() {
+				if fv.IsValid() && fv.CanSet() {
+					//if fv.IsValid() && fv.CanSet() {
+					if d, ok := ft.Tag.Lookup("default"); ok && d != "" {
+						//fmt.Print(d)
+						if fv.Kind() == reflect.String {
+							fv.SetString(d)
+						} else if fv.Kind() == reflect.Bool {
+							fv.SetBool(mustBe(strconv.ParseBool(d)).(bool))
+						} else if fv.Kind() == reflect.Int ||
+							fv.Kind() == reflect.Int8 ||
+							fv.Kind() == reflect.Int16 ||
+							fv.Kind() == reflect.Int32 ||
+							fv.Kind() == reflect.Int64 {
+							fv.SetInt(mustBe(strconv.ParseInt(d, 0, 64)).(int64))
+						} else if fv.Kind() == reflect.Uint ||
+							fv.Kind() == reflect.Uint8 ||
+							fv.Kind() == reflect.Uint16 ||
+							fv.Kind() == reflect.Uint32 ||
+							fv.Kind() == reflect.Uint64 {
+							fv.SetUint(mustBe(strconv.ParseUint(d, 0, 64)).(uint64))
+						} else if fv.Kind() == reflect.Float32 ||
+							fv.Kind() == reflect.Float64 {
+							fv.SetFloat(mustBe(strconv.ParseFloat(d, 64)).(float64))
+						} else if d == "null" {
+							//fv.Set(reflect.New(fv.Type()).Elem())
+							fv.Set(reflect.New(ft.Type).Elem())
+						}
 					}
 				}
 			}
@@ -148,83 +160,43 @@ func rec(v reflect.Value, iskey bool, ind int) {
 			if tagName == "" {
 				tagName = ft.Name
 			}
+			//fmt.Println("+++++++++++ tagNmae", ft.Type, tagName)
 
+			hasElem = true
+			fmt.Println()
 			indent(true, ind+1, reflect.ValueOf(tagName))
 			fmt.Print(": ")
 			//fmt.Print(reflect.DeepEqual(fv.Interface(), reflect.Zero(fv.Type()).Interface()))
 			rec(fv, false, ind+1)
 
 			if i < v.NumField()-1 {
-				fmt.Println(",")
+				fmt.Print(",")
 			}
 		}
-		fmt.Println()
-		indent(true, ind, reflect.ValueOf("}"))
+		if hasElem {
+			fmt.Println()
+		}
+		//fmt.Println("++++++++++", v.IsZero())
+		indent(hasElem, ind, reflect.ValueOf("}"))
+
+	case reflect.String:
+		indent(iskey, ind, reflect.ValueOf(stringify(v.String())))
 
 	default:
-		fmt.Println("default", v)
+		indent(iskey, ind, v)
 	}
 }
 
-func prettyPrint(obj interface{}) {
+func prettyPrint(obj Interface, args ...interface{}) {
+	if len(args) > 1 {
+		fmt.Println("[Warning] Currently only one optional argument is supported and it is the indentation string")
+	}
+	if len(args) == 1 {
+		if indStr, ok := args[0].(string); ok {
+			indentationStr = indStr
+		}
+	}
+
 	rec(reflect.ValueOf(obj), true, 0)
-}
-
-func main() {
-	type A map[interface{}]interface{}
-	obj := A{
-		1:   11,
-		"a": 0,
-		2:   map[int]string{1: "b"},
-		4: map[bool]interface{}{
-			true: map[string]int{
-				"c": 3,
-			},
-		},
-		5: map[bool]interface{}{
-			true: map[string]int{
-				"d": 5,
-			},
-		},
-		"b": "b1",
-	}
-
-	//prettyPrint(interface{}(obj))
-
-	obj = A{
-		2: map[int]string{1: "b"},
-		4: map[bool]interface{}{
-			true: map[string]int{
-				"c": 3,
-			},
-		},
-		5: map[bool]interface{}{
-			true: map[string]int{
-				"d": 5,
-			},
-		},
-		"a": &struct {
-			HelloBool  bool                        `json:"hello_bool, omitempty" default:"true"`
-			Hello1Bool bool                        `json:"hello1_bool, omitempty" default:"false"`
-			Hello2Bool bool                        `json:"hello2_bool, omitempty" default:"t"`
-			Hello3Bool bool                        `json:"hello3_bool, omitempty" default:"T"`
-			HelloInt   int8                        `json:"hello_int, omitempty" default:"-0b101"`
-			HelloUint  uint                        `json:"hello_uint, omitempty" default:"0b101"`
-			HelloFloat float64                     `json:"hello_float, omitempty" default:"0.5"`
-			World      string                      `json:"world,omitempty" default:"ten teneeee"`
-			Tada       bool                        `json:"-" default:"true"`
-			Horray     map[interface{}]interface{} `json:"horray" default:"null"`
-			Horray1    map[interface{}]interface{} `json:"horray1,omitempty" default:"null"`
-			Hey        int                         `json:"hey"`
-		}{
-			//Hello: 1,
-			//World: "World",
-			Tada: false,
-		},
-	}
-	prettyPrint(interface{}(obj))
-	//map[interface{}]interface{}{
-	//
-	//}
-
+	fmt.Println()
 }
